9faa5d680e92c34c2269167bf96dc23519e6e5aa
Daniel Meskin <github@dmeskin.xyz>
2021-05-01 00:25:17 -0400

    Fix some style issues

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,15 +1,14 @@
 escapeURL = "https://en.wikipedia.org/wiki/Dachshund";
-backURL = "https://www.google.com/search?q=dachshund"
+backURL = "https://www.google.com/search?q=dachshund";
 
 function escape() {
-	window.open(escapeURL)
+	window.open(escapeURL);
 	location.replace(backURL);
 }
 window.onload = function() {
-	preloadEscape='<link rel="prefetch" href="' + escapeURL + '" as="document">'
-	preloadBack='<link rel="prefetch" href="' + backURL + '" as="document">'
-	console.log(preloadEscape,preloadBack)
-	document.getElementsByTagName('head')[0].innerHTML+=(preloadEscape)
+	preloadEscape=('<link rel="prefetch" href="' + escapeURL + '" as="document">');
+	preloadBack=('<link rel="prefetch" href="' + backURL + '" as="document">');
+	document.getElementsByTagName('head')[0].innerHTML+=(preloadEscape);
 	document.getElementsByTagName('head')[0].innerHTML+=(preloadBack);
 	window.onkeydown = function(key) {
 		if (key.keyCode == 27) {
