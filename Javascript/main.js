58a6cc41970746f75565202b186574d2856071cf
Daniel Meskin <github@dmeskin.xyz>
2021-05-01 00:23:38 -0400

    Add prefetching

--- Javascript/main.js
+++ Javascript/main.js
@@ -6,6 +6,11 @@ function escape() {
 	location.replace(backURL);
 }
 window.onload = function() {
+	preloadEscape='<link rel="prefetch" href="' + escapeURL + '" as="document">'
+	preloadBack='<link rel="prefetch" href="' + backURL + '" as="document">'
+	console.log(preloadEscape,preloadBack)
+	document.getElementsByTagName('head')[0].innerHTML+=(preloadEscape)
+	document.getElementsByTagName('head')[0].innerHTML+=(preloadBack);
 	window.onkeydown = function(key) {
 		if (key.keyCode == 27) {
 			escape();
